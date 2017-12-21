3a6c52bd5f787b3ff54d099e3029f21bc8e3bb12
emenkens <elias.menkens@gmail.com>
2017-12-21 03:01:05 +0100

    Added Swedish translation

---
+++ publishable/translations/sv/confirmation.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'email-title' => 'E-postbekräftelse',
+    'email-intro'=> 'Klicka på knappen nedan för att bekräfta din e-postadress.',
+    'email-button' => 'Bekräfta e-postadress',
+    'message' => 'Tack för registreringen! Vänligen kontrollera din inkorg.',
+    'success' => 'Du har nu verifierat ditt konto! Nu kan du logga in.',
+    'again' => 'Du måste bekräfta tin e-postadress innan du kan komma åt den här sidan. ' .
+                '<br>Om du inte har mottagit någon bekräftelse via e-post än, vänligen kontrollera din skräppost.'.
+                '<br>För att skicka bekräftelsen på nytt, vänligen <a href="' . url('confirmation/resend') . '" class="alert-link">klicka här</a>.',
+    'resend' => 'En s-postbekräftelse har skickats till dig. Vänligen kontrollera din inkorg.'
+];
