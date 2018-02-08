7ecf7a66311e60c2e0051a2e493684d7cac19249
Dominik Kohler <dominik.kohler@komail.ch>
2018-02-08 10:04:44 +0100

    Fixed some typos in german translation.

--- publishable/translations/de/confirmation.php
+++ publishable/translations/de/confirmation.php
@@ -4,10 +4,10 @@ return [
     'email-title' => 'E-Mail-Verifizierung',
     'email-intro'=> 'Um die E-Mail-Adresse zu verifizieren, klicken Sie auf die Schaltfläche unten',
     'email-button' => 'E-Mail verifizieren',
-    'message' => 'Danke für Ihre Anmeldung. Wir haben Ihnen soeben eine Bestätigungs- E-Mail zugeschickt.',
-    'success' => 'Sie haben Ihren Account wurde erfolgreich verifiziert. Sie können sich nun einloggen.',
+    'message' => 'Danke für Ihre Anmeldung. Wir haben Ihnen soeben eine Bestätigungs-E-Mail geschickt.',
+    'success' => 'Sie haben Ihren Account erfolgreich verifiziert. Sie können sich nun einloggen.',
     'again' => 'Sie müssen Ihre E-Mail-Adresse verifizieren, bevor Sie Zugang zu dieser Seite erhalten. ' .
                 '<br>Wenn Sie die Bestätigungs-E-Mail nicht erhalten haben, prüfen Sie bitte Ihren Spam-Ordner.'.
                 '<br>Um eine neue Bestätigungs-E-Mail zu erhalten, <a href="' . url('confirmation/resend') . '" class="alert-link">klicken Sie hier</a>.',
-    'resend' => 'Wir haben Ihnen eine neue Bestätigungs-E-Mail zugeschickt.'
+    'resend' => 'Wir haben Ihnen eine neue Bestätigungs-E-Mail geschickt.'
 ];
