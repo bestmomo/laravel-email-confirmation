1e86eeac13f8f158f444abbea148fb28d1cb6606
emenkens <elias.menkens@gmail.com>
2018-03-21 00:11:47 +0100

    Update confirmation.php
    Correct spelling errors

--- publishable/translations/sv/confirmation.php
+++ publishable/translations/sv/confirmation.php
@@ -6,8 +6,8 @@ return [
     'email-button' => 'Bekräfta e-postadress',
     'message' => 'Tack för registreringen! Vänligen kontrollera din inkorg.',
     'success' => 'Du har nu verifierat ditt konto! Nu kan du logga in.',
-    'again' => 'Du måste bekräfta tin e-postadress innan du kan komma åt den här sidan. ' .
+    'again' => 'Du måste bekräfta din e-postadress innan du kan komma åt den här sidan. ' .
                 '<br>Om du inte har mottagit någon bekräftelse via e-post än, vänligen kontrollera din skräppost.'.
                 '<br>För att skicka bekräftelsen på nytt, vänligen <a href="' . url('confirmation/resend') . '" class="alert-link">klicka här</a>.',
-    'resend' => 'En s-postbekräftelse har skickats till dig. Vänligen kontrollera din inkorg.'
+    'resend' => 'En e-postbekräftelse har skickats till dig. Vänligen kontrollera din inkorg.'
 ];
